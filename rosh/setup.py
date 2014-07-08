8e013f666e6258c5b52fdcdfb6d1b0cfe239695e
Dan Lazewatsky <dan@lazewatsky.com>
2014-07-08 11:07:07 -0500

    make sure rosh.impl gets installed

--- rosh/setup.py
+++ rosh/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['rosh'],
+    packages=['rosh', 'rosh.impl'],
     package_dir={'': 'src'},
     scripts=['rosh'],
     requires=['genmsg', 'genpy', 'roslib', 'rospkg']
